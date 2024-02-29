a19e03b89021eccebd4821c08b3f2d2952af8ce1
pnabiniw <pnabiniw@gmail.com>
2024-02-29 16:22:49 +0545

    ORMs

---
+++ crud/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.FileField(upload_to='profile_pictures', null=True, blank=True)
+    address = models.CharField(max_length=20)
+    phone = models.CharField(max_length=14)
+
+    def __str__(self):
+        return f'profile of {self.user.username}'
+
+
+class ClassRoom(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class Student(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='user_students')
+    classroom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE, related_name="classroom_students")
--- myapp/models.py
+++ myapp/models.py
@@ -7,3 +7,12 @@ class ClassRoom(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    classroom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE, related_name='classroom_students')
+
+    def __str__(self):
+        return self.name
