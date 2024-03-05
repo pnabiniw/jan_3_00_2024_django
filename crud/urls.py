320053e00bc879b8075a82df174c549f96f35f44
pnabiniw <pnabiniw@gmail.com>
2024-03-05 16:23:59 +0545

    user register

--- crud/urls.py
+++ crud/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import signup
+from .views import signup, user_login, classroom
 
 
 urlpatterns = [
-    path('signup/', signup, name="signup")
+    path('signup/', signup, name="signup"),
+    path('login/', user_login, name="user_login"),
+    path("classroom/", classroom, name="crud_classroom")
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def signup(request):
+    print(request.method)
+    if request.method == "POST":
+        username = request.POST.get("username")
+        fn = request.POST.get("first_name")
+        ln = request.POST.get("last_name")
+        email = request.POST.get("email")
+        pw1 = request.POST.get('password1')
+        pw2 = request.POST.get('password2')
+
+        if pw1 != pw2:
+            messages.error(request, "Password didn't match!!")
+            return redirect('signup')
+        user = User.objects.create(username=username, first_name=fn, last_name=ln,
+                                   email=email, is_active=True)
+        user.set_password(pw1)
+        user.save()
+        messages.success(request, "User created successfully!!")
+        return redirect('signup')
+    # This is handling a get request
     return render(request, template_name="crud/signup.html")
+
+
+def user_login(request):
+    return render(request, template_name="crud/login.html")
+
+
+def classroom(request):
+    return render(request, template_name="crud/classroom.html")
